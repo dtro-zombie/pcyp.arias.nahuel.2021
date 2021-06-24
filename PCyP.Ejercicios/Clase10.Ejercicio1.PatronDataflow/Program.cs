9ebcca8bf68955ba4c10ff6e3d34b952018e4702
nahuel arias <ariasnahuel8@gmail.com>
2021-06-24 18:18:52 -0300

    commit final

---
+++ PCyP.Ejercicios/Clase10.Ejercicio1.PatronDataflow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase10.Ejercicio1.PatronDataflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var df1 = new TransformBlock<int, int>(t =>
+            {
+                Console.WriteLine("Entered 1st DF");
+                Thread.Sleep(3000);
+                return 2;
+            });
+
+            var df2 = new ActionBlock<int>(t =>
+            {
+
+                Console.WriteLine("Entered 2nd Task");
+                Thread.Sleep(2000);
+                Console.WriteLine(t);
+            });
+
+            df1.LinkTo(df2);
+
+            df1.Completion.ContinueWith(t =>
+            df2.Complete());
+
+            df1.Post(2);
+
+            df2.Completion.Wait();
+        }
+
+    }
+}
---
+++ PCyP.Ejercicios/Clase9.Ejercicio1.WaitAllOneByOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clase9.Ejercicio1.WaitAllOneByOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            //lista de tareas
+            Task<int>[] tasks = new Task<int>[3];
+            //Ejecutar tarea
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+            //mientras el largo de las tareas sea mayor a 0
+            while (tasks.Length > 0)
+            {
+                //guarda la tarea que termina primero en i
+                int i = Task.WaitAny(tasks);
+                //la tarea completada la guardo en un  variable task
+                Task<int> completedTask = tasks[i];
+                //el resultado de la task lo sumo en sum
+                Console.WriteLine(sum+=completedTask.Result);
+                //hago un arrary temporal que copia atask
+                var temp = tasks.ToList();
+                //remuevo la task del del indice i
+                temp.RemoveAt(i);
+                //igualo task con la array temporal
+                tasks = temp.ToArray();
+            }
+            Console.ReadLine();
+        }
+    }
+}
